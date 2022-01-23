6d31abee117e17c4e6e7bd448fd6af5a546b53fa
FloRad <florad92@gmail.com>
2022-01-24 00:41:07 +0100

    finished moral code viewer 1.0

--- src/module/MoralCodeViewer.js
+++ src/module/MoralCodeViewer.js
@@ -1,13 +1,25 @@
 export class MoralCodeViewer extends FormApplication {
+  constructor(object, options = {}) {
+    super(object, options);
+  }
+
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      title: game.i18n.localize('MC.ViewerTitle'),
       template: 'modules/moral-code/templates/viewer.hbs',
       classes: ['moral-code'],
-      width: 500,
-      height: 600,
+      width: 700,
+      height: 700,
       resizable: false,
+      closeOnSubmit: false,
     });
+  }
+
+  get id() {
+    return this.actor.uuid + '-moral-code';
+  }
+
+  get title() {
+    return game.i18n.format('MC.ViewerTitle', { actor: this.actor.name });
   }
 
   get actor() {
@@ -16,19 +28,49 @@ export class MoralCodeViewer extends FormApplication {
 
   activateListeners(html) {
     super.activateListeners(html);
+    html.find('footer .randomize').on('click', this.randomizeMoralCode.bind(this));
+    html.find('footer .reset').on('click', this.resetMoralCode.bind(this));
   }
 
   async getData() {
     const data = super.getData();
+    data.code = this.actor.getFlag('moral-code', 'code');
     return data;
   }
 
   /**
-   *
    * @param {Event} event The initial triggering submission event
    * @param {object} formData The object of validated form data with which to update the object
    */
   async _updateObject(_event, formData) {
-    console.log(formData);
+    await this.actor.setFlag('moral-code', 'code', formData);
+  }
+
+  async randomizeMoralCode() {
+    const code = {
+      'selfish-selfless': await this.randomMoral(),
+      'practical-idealistic': await this.randomMoral(),
+      'responsible-capricious': await this.randomMoral(),
+      'lawful-anarchic': await this.randomMoral(),
+      'tolerant-intolerant': await this.randomMoral(),
+      'progressive-traditional': await this.randomMoral(),
+    };
+    console.log(code);
+    await this.actor.setFlag('moral-code', 'code', code);
+    return this.render(true);
+  }
+
+  /**
+   * @returns {number} a random value for a moral code secton
+   */
+  async randomMoral() {
+    const formula = '1d4';
+    const roll = await new Roll(formula).evaluate({ async: true });
+    return roll.total;
+  }
+
+  async resetMoralCode() {
+    await this.actor.unsetFlag('moral-code', 'code');
+    return this.render(true);
   }
 }
