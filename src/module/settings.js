3c8129cb417455d50ed238fcee1a335c67ad925f
FloRad <florad92@gmail.com>
2022-02-17 22:26:52 +0100

    added solarized themes

--- src/module/settings.js
+++ src/module/settings.js
@@ -7,6 +7,8 @@ export function registerSettings() {
     type: String,
     choices: {
       classic: 'MC.Theme.Classic',
+      solarized: 'MC.Theme.Solarized',
+      'solarized-dark': 'MC.Theme.SolarizedDark',
     },
     default: 'classic',
   });
